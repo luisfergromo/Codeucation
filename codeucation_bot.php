b9d8d193444639aac5e352bf52acc6809c9b3931
Luis Fernando Gutiérrez Romo <changoluis@hotmail.com>
2016-06-02 11:34:49 -0500

    Update codeucation_bot.php

--- codeucation_bot.php
+++ codeucation_bot.php
@@ -1,1 +1,9 @@
+<?php
+$botToken = "224238361:AAFViBooLN91Ob1FJQDQR-DypG4uPqdD8Y8";
+$website = "https://api.telegram.org/bot".$botToken;
 
+$update = file_get_contents($website."/getupdates");
+
+print_r($update);
+
+?>
